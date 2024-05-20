36804b295d816ff07322422ab518f54887b79415
Andrés <abblancodos@gmail.com>
2024-05-20 14:52:24 -0600

    Hazard y forwarding unit preliminar

--- Proyecto_2/forwarding_unit.sv
+++ Proyecto_2/forwarding_unit.sv
@@ -6,4 +6,33 @@ module forwarding_unit (
     
 //logica
 
+always_comb begin
+    {FORWARD_A, FORWARD_B} = 4'b0000;
+    if (
+        REGWRITE_EX_MEM
+        && (ARD_EX_MEM != 0)
+        && (ARD_EX_MEM == ARS1)
+    ) FORWARD_A = 2'b10;
+    else if (
+        REGWRITE_EX_MEM
+        && (ARD_EX_MEM != 0)
+        && (ARD_EX_MEM  == ARS2)
+    ) FORWARD_B = 2'b10;
+    else if (
+        ARD_MEM_WB
+        && (ARD_MEM_WB != 0)
+        && !(REGWRITE_EX_MEM && (ARD_EX_MEM != 0))
+        && (ARD_EX_MEM == ARS1)
+        && (ARD_MEM_WB == ARS1)
+    ) FORWARD_A = 2'b01;
+    else if (
+        ARD_MEM_WB
+        && (ARD_MEM_WB != 0)
+        && !(REGWRITE_EX_MEM && (ARD_EX_MEM != 0))
+        && (ARD_EX_MEM == ARS2)
+        && (ARD_MEM_WB == ARS2)
+    ) FORWARD_A = 2'b01;
+
+end
+
 endmodule
--- Proyecto_2/hazard_unit.sv
+++ Proyecto_2/hazard_unit.sv
@@ -1,10 +1,24 @@
 module hazard_unit (
-    input wire MEMREAD_ID_EX,
+    input wire MEMREAD_ID_EX, BEQ_WRONG_PRED,
     input wire [4:0] ARS1_IF_ID, ARS2_IF_ID, ARD_ID_EX,
 
-    output wire STALL, MUX_SEL
+    output wire STALL, MUX_SEL, PCWRITE
 );
-    
+
 //logica
+
+always_comb begin
+
+    if (
+        MEMREAD_ID_EX
+        && ((ARD_ID_EX == ARS1_IF_ID) || (ARD_ID_EX == ARS2_IF_ID))
+    ) {STALL, MUX_SEL} = 2'b11;
+    else
+        {STALL, MUX_SEL} = 2'b00;
+
+    if(BEQ_WRONG_PRED) PCWRITE = 1'b1;
+    else PCWRITE = 1'b0;
+
+end
 
 endmodule
